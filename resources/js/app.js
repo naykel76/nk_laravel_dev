a1e190be4807a6871543b6a5d59a94bdd11d01ca
Nathan Watts <nathan@naykel.com.au>
2021-10-30 07:31:21 +1000

    updated for alpinejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,7 @@
 require('./bootstrap');
 
-import { createApp } from 'vue'
-
-// import DatePicker from './components/DatePicker.vue'
-import Flash from './components/Flash.vue'
-import ImagePicker from './components/ImagePicker.vue'
-import Modal from './components/Modal.vue'
-import Sidebar from './components/Sidebar.vue'
-createApp({
-    components: {
-        Flash, ImagePicker, Modal, Sidebar,
-    },
-    data() {
-        return {
-            showModal: false,
-            showSidebar: false,
-        };
-    },
-}).mount('#app');
-
+import Alpine from 'alpinejs'
+ 
+window.Alpine = Alpine
+ 
+Alpine.start()
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .vue()
     .sass('resources/scss/app.scss', 'public/css/nk_jtb.css')
     .sass('resources/scss/jtb.scss', 'public/css/styles.css');
 
